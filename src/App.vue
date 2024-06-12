a6a07baecbc1a1e215693457bf664026bb22314f
ryusei-engineer <ryusei.engineer@gmail.com>
2024-06-12 22:38:07 +0900

    ハンドラー

--- src/App.vue
+++ src/App.vue
@@ -3,13 +3,17 @@ import HelloWorld from './components/HelloWorld.vue'
 import TheWelcome from './components/TheWelcome.vue'
 import { ref } from 'vue'
 let count = ref(0);
-function increment(){
-  count.value++;
+function increment(num){
+  count.value = num;
 }
 </script>
 
 <template>
 {{ count }} 
-<button @click="increment"></button>
+<button @click="increment(1)">1</button>
+<button @click="increment(2)">2</button>
+<button @click="increment(3)">3</button>
+<button @click="increment(4)">4</button>
+<button @click="increment(5)">5</button>
 </template>
 
